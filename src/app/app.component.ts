646255161d22022dd10cfe1b5967fa735c773b46
Siddharth Venkatesh <siddharth.venkatesh@astria.co.in>
2020-03-13 13:11:58 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'single-spa-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ng9-single-spa';
+}
